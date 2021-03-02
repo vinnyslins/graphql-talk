62683461d4158809e3c08a2e24fc93ba624b9855
Vinnys Lins <vinnysflins@gmail.com>
2021-03-02 03:45:09 -0300

    feat: setup apollo server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('Hello, GraphQL!!!')
+const { ApolloServer, gql } = require('apollo-server')
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin'
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster'
+  }
+]
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
